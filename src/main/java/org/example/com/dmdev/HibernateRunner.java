d7aae33c89ee9c8d0d71a101aef43e7b0d1d9c88
rekynkovav <rekynkovav@yandex.ru>
2025-04-28 21:43:11 +0300

    a

--- src/main/java/org/example/com/dmdev/HibernateRunner.java
+++ src/main/java/org/example/com/dmdev/HibernateRunner.java
@@ -1,21 +1,33 @@
 package org.example.com.dmdev;
 
+
+import org.example.com.dmdev.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class HibernateRunner {
-    public static void main (String[] args) throws SQLException {
+    public static void main(String[] args) {
         Configuration configuration = new Configuration();
+//        configuration.addAnnotatedClass(User.class);
         configuration.configure();
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
 
+            User user = User.builder()
+                    .username("asd@mail.com")
+                    .firstname("Ivan")
+                    .lastname("Ivanov")
+                    .birthDate(LocalDate.of(2000,1,19))
+                    .age(20)
+                    .build();
+            session.save(user);
+
+            session.getTransaction().commit();
         }
-        System.out.println("OK");
     }
 }

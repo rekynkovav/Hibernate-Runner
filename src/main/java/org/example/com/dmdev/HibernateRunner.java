b562ce04c29608364093f6f41e7f6a8816ed5009
rekynkovav <rekynkovav@users.noreply.github.com>
2025-04-28 19:45:36 +0300

    1

--- src/main/java/org/example/com/dmdev/HibernateRunner.java
+++ src/main/java/org/example/com/dmdev/HibernateRunner.java
@@ -1,5 +1,21 @@
 package org.example.com.dmdev;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class HibernateRunner {
-    аааааа
+    public static void main (String[] args) throws SQLException {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+             Session session = sessionFactory.openSession()) {
+
+        }
+        System.out.println("OK");
+    }
 }
